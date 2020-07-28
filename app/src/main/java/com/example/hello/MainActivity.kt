81bef6586a9a7a45a956b900c2c2136bc78ebe8c
STELLAMARUH <stellamaruh@gmail.com>
2020-07-28 11:42:20 +0300

    initial commit

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.hello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnLogin.SetOnClickListener {
+            var userName = etUsername.text.toString()
+            var password = etPassword.text.toString()
+            Toast.makeText(baseContext, userName, Toast.LENGTH_LONG).show()
+        }
     }
+    }
+
+private fun Button.SetOnClickListener(function: () -> Unit) {
+    TODO("Not yet implemented")
 }
+
+
